6beb9c2d0a4f13555fcc589c46246d6799eb7b55
David Oliveira <dol.jpo@gmail.com>
2021-06-08 08:31:46 -0300

    Convert on typing, trying some validations

--- src/bin2dec.js
+++ src/bin2dec.js
@@ -1,3 +1,6 @@
+const BINARY_INPUT = document.getElementById('binary');
+const RESULT = document.getElementById('result');
+const BINARY_MAX_LENGTH = 8;
 const VALID_KEYS = [
 	'0',
 	'1',
@@ -11,22 +14,55 @@ const VALID_KEYS = [
 	'F5'
 ]
 
-document.addEventListener('keydown', e => {
-	if (!VALID_KEYS.includes(e.key)) {
-		e.preventDefault();
+BINARY_INPUT.addEventListener('keydown', e => {
+	if (isValidKey(e.key)) {
+		hideWarning();
+
+		if (inputHasValidLength(e.key))
+			validateInput(e.key);
 	}
 	else {
-		validateInput(e.key);
+		showWarning(e.key);
+		e.preventDefault();
 	}
 });
 
-function validateInput(digit) {
-	let binary = document.getElementById('binary').value + digit;
-	if (binary.length > 8) return;
+function isValidKey(character) {
+	return VALID_KEYS.includes(character);
 }
 
-function convert() {
-	let binary = document.getElementById('binary').value;
+function inputHasValidLength(character) {
+	if (isBinary(character)) {
+		return (BINARY_INPUT.value.length + 1) <= BINARY_MAX_LENGTH;
+	}
+
+	return true;
+}
+
+function isBinary(character) {
+	return character == '1' || character == '0';
+}
+
+function showWarning(character) {
+	if (character == '1' || character == '0') {
+		document.getElementById('warning').innerText = "Converter max length of binary reached (" + BINARY_MAX_LENGTH + " digits)";
+	} else {
+		document.getElementById('warning').innerText = "Invalid character '" + character + "' (binary accepts only 1 and 0)";	
+	}
+}
+
+function hideWarning() {
+	document.getElementById('warning').innerText = '';	
+}
+
+function validateInput(digit) {
+	let binary = BINARY_INPUT.value + digit;
+	if (binary.length > BINARY_MAX_LENGTH) return;
+
+	convert(binary);
+}
+
+function convert(binary) {	
 	let decimal = 0;
 	let pow = binary.length - 1;
 
@@ -37,10 +73,11 @@ function convert() {
 		pow--;
 	}
 
-	document.getElementById('result').innerText = decimal;
+	RESULT.innerText = decimal;
 }
 
 function reset() {
-	document.getElementById('binary').value = '';
-	document.getElementById('result').innerText = '';
+	BINARY_INPUT.value = '';
+	RESULT.innerText = '';
+	hideWarning();
 }

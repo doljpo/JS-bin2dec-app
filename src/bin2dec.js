a300f6641a076c79128156928e6413d8f0a17fb6
David Oliveira <dol.jpo@hotmail.com>
2021-07-18 11:03:59 -0300

    valid keys removed, did some refactoring

--- src/bin2dec.js
+++ src/bin2dec.js
@@ -1,45 +1,31 @@
 const BINARY_INPUT = document.getElementById('binary');
 const RESULT = document.getElementById('decimal');
+const WARNING = document.getElementById('warning');
 const BINARY_MAX_LENGTH = 8;
-const VALID_KEYS = [
-	'0',
-	'1',
-	'Enter',
-	'Backspace',
-	'Delete',
-	'Tab',
-	'F5',
-	'Alt',
-	'ArrowLeft',
-	'ArrowRight'
-]
 
 BINARY_INPUT.addEventListener('keydown', e => {
 	hideWarning();
-	let pressedKey = e.key;
-		
-	if (maxLengthNotReached(pressedKey) && isValidInput(pressedKey)) {
-		computeInput(pressedKey);
+	
+	if (isValidInput(e.key)) {
+		convert(e.key);
 	} else {
-		showWarning(pressedKey);
 		e.preventDefault();
 	}
 });
 
-function computeInput(input) {
-	if (isBinary(input)) {
-		let binary = BINARY_INPUT.value + input;
-		convert(binary);
+function hideWarning() {
+	WARNING.innerText = '';	
+}
+
+function isValidInput(pressedKey) {
+	if (!isBinary(pressedKey)) {
+		WARNING.innerText = "Invalid character '" + pressedKey + "' (binary accepts only 1 and 0)";	
+		return false;
 	}
-}
 
-function isValidInput(input) {
-	return VALID_KEYS.includes(input);
-}
-
-function maxLengthNotReached(pressedKey) {
-	if (isBinary(pressedKey)) {
-		return (BINARY_INPUT.value.length + 1) <= BINARY_MAX_LENGTH;
+	if (maxLengthReached()) {
+		WARNING.innerText = "Maximum length reached (" + BINARY_MAX_LENGTH + " digits)";
+		return false;
 	}
 
 	return true;
@@ -49,24 +35,17 @@ function isBinary(pressedKey) {
 	return pressedKey.match(/^[0-1]+$/g);
 }
 
-function showWarning(pressedKey) {	
-	if (isBinary(pressedKey)) {
-		document.getElementById('warning').innerText = "Max length reached (" + BINARY_MAX_LENGTH + " digits)";
-	} else if (!isValidInput(pressedKey)) {
-		document.getElementById('warning').innerText = "Invalid character '" + pressedKey + "' (binary accepts only 1 and 0)";	
-	}
-}
-
-function hideWarning() {
-	document.getElementById('warning').innerText = '';	
+function maxLengthReached() {
+	return (BINARY_INPUT.value.length + 1) >= BINARY_MAX_LENGTH;
 }
 
 function convert(pressedKey) {	
+	let binary = BINARY_INPUT.value + pressedKey;
 	let decimalValue = 0;
-	let pow = pressedKey.length - 1;
+	let pow = binary.length - 1;
 
-	for (let index = 0; index < pressedKey.length; index++) {
-		const digit = pressedKey[index];		
+	for (let index = 0; index < binary.length; index++) {
+		const digit = binary[index];		
 		decimalValue += (digit * Math.pow(2, pow));
 		pow--;
 	}
